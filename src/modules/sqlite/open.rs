1a07abcf4a9c706aa672a9e1a5087a9bbae12bf0
Emile Fugulin <code@efugulin.com>
2024-09-09 21:40:31 -0400

    Add more options

--- src/modules/sqlite/open.rs
+++ src/modules/sqlite/open.rs
@@ -1,4 +1,7 @@
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{
+    sync::atomic::{AtomicUsize, Ordering},
+    time::Duration,
+};
 
 use rquickjs::{Ctx, FromJs, Object, Result, Value};
 use sqlx::sqlite::{SqliteConnectOptions, SqlitePoolOptions};
@@ -9,6 +12,9 @@ static IN_MEMORY_DB_SEQ: AtomicUsize = AtomicUsize::new(0);
 
 pub async fn open(options: OpenOptions) -> Result<Database> {
     let mut connect_options = SqliteConnectOptions::new();
+    connect_options = connect_options
+        .foreign_keys(true)
+        .thread_name(|id| format!("quickjs-sqlite-worker-{id}"));
     if let Some(filename) = options.filename {
         connect_options = connect_options.filename(filename).create_if_missing(true);
     }
@@ -26,7 +32,7 @@ pub async fn open(options: OpenOptions) -> Result<Database> {
     let mut pool_options = SqlitePoolOptions::new();
     pool_options = pool_options
         .idle_timeout(None)
-        .max_lifetime(None)
+        .max_lifetime(Some(Duration::from_secs(60 * 60)))
         .max_connections(5)
         .min_connections(0);
 
