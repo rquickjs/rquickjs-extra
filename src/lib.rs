3dd8708b8830ee37291e13f83ee89f17062668fd
Emile Fugulin <code@efugulin.com>
2024-11-18 22:45:04 -0500

    Add os to main crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #[cfg(feature = "console")]
 pub use rquickjs_extra_console as console;
 
+#[cfg(feature = "os")]
+pub use rquickjs_extra_os as os;
+
 #[cfg(feature = "sqlite")]
 pub use rquickjs_extra_sqlite as sqlite;
 
