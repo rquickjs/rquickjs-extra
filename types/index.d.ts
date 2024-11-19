07c95ac788d97619ccda1a27e0b26e3f2832dd4d
Emile Fugulin <code@efugulin.com>
2024-11-18 23:33:24 -0500

    Add missing typing import

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="console.d.ts" />
+/// <reference path="globals.d.ts" />
 /// <reference path="os.d.ts" />
 /// <reference path="sqlite.d.ts" />
 /// <reference path="timers.d.ts" />
